339bf85fb73997643082792b0ddcaf34bd2c1a99
sanketkadge1 <sanketkadge@gmail.com>
2022-07-19 11:56:57 +0530

    Update versions.tf

--- Modules/S3/versions.tf
+++ Modules/S3/versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 1.2.4"
+  required_version = ">= 0.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0"
+      version = ">= 4.5"
     }
   }
 }
